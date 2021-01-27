6100f1eef76988eb5fb0f646f7236d64f3854ce0
clarice99 <claricedsilva99@gmail.com>
2021-01-27 20:38:21 +0530

    Object_Detection added

--- gods-eye/src/components/Detections.js
+++ gods-eye/src/components/Detections.js
@@ -1,36 +1,111 @@
-import React from 'react'
-const DetectionPage = () => {
+import React from "react";
+import ReactDOM from "react-dom";
 
-  //Disable Right click
-  if (document.addEventListener) {
-    document.addEventListener('contextmenu', function (e) {
-      e.preventDefault();
-    }, false);
-  }
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs";
+import "./Detections.css";
 
-  // Alert on Tab Changed within the Same browser Window
-  function handleVisibilityChange() {
-    if (document.hidden) {
-      alert("You changed tab within the same browser window");
-      // the page is hidden
-    } else {
-      // the page is visible
+class Detection extends React.Component {
+  videoRef = React.createRef();
+  canvasRef = React.createRef();
+
+  componentDidMount() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const webCamPromise = navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            facingMode: "user"
+          }
+        })
+        .then(stream => {
+          window.stream = stream;
+          this.videoRef.current.srcObject = stream;
+          return new Promise((resolve, reject) => {
+            this.videoRef.current.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        });
+      const modelPromise = cocoSsd.load();
+      Promise.all([modelPromise, webCamPromise])
+        .then(values => {
+          this.detectFrame(this.videoRef.current, values[0]);
+        })
+        .catch(error => {
+          console.error(error);
+        });
     }
   }
 
-  document.addEventListener("visibilitychange", handleVisibilityChange, false);
 
-  return (<div>
-    <center>
-      <h3>
-        Detections Page
-        </h3>
-      <small>
-        Add Object and Face Detections Here
-        </small>
-    </center>
-  </div>
-  )
+
+
+  detectFrame = (video, model) => {
+    model.detect(video).then(predictions => {
+      this.renderPredictions(predictions);
+      requestAnimationFrame(() => {
+        this.detectFrame(video, model);
+      });
+    });
+  };
+
+  renderPredictions = predictions => {
+    const ctx = this.canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // Font options.
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      const width = prediction.bbox[2];
+      const height = prediction.bbox[3];
+      // Draw the bounding box.
+      ctx.strokeStyle = "#00FFFF";
+      ctx.lineWidth = 4;
+      ctx.strokeRect(x, y, width, height);
+      // Draw the label background.
+      ctx.fillStyle = "#00FFFF";
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10); // base 10
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    });
+
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      // Draw the text last to ensure it's on top.
+      ctx.fillStyle = "#000000";
+      ctx.fillText(prediction.class, x, y);
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <video
+          className="size"
+          autoPlay
+          playsInline
+          muted
+          ref={this.videoRef}
+          width="600"
+          height="500"
+        />
+        <canvas
+          className="size"
+          ref={this.canvasRef}
+          width="600"
+          height="500"
+        />
+      </div>
+    );
+  }
 }
 
-export default DetectionPage;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Detection />, rootElement);
+
+export default Detection;
