8fe9e5e267b28b990bd2881bc39d63d5f4fa5aeb
Sancia Dcunha <sanciadcunha15@gmail.com>
2021-03-29 14:38:20 +0530

    UPDATED: Countdown timer json file, dashboard UI

--- gods-eye/src/components/Dashboard.js
+++ gods-eye/src/components/Dashboard.js
@@ -5,6 +5,9 @@ import Detection from './Detections';
 import { Button } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import swal from 'sweetalert';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+const minuteSeconds = 60;
+const hourSeconds = 3600;
 
 // const WebcamComponent = () => <Webcam />;
 const Dashboard = () => {
@@ -56,33 +59,102 @@ const Dashboard = () => {
     }
   });
 
-  return (
+  //For timer
+  const timerProps = {
+    isPlaying: true,
+    size: 120,
+    strokeWidth: 6
+  };  
+  const renderTime = (dimension, time) => {
+    return (
+      <div className="time-wrapper">
+        <div className="time">{time}</div>
+        <div>{dimension}</div>
+      </div>
+    );
+  };
+//For minutes, seconds
+const getTimeSeconds = (time) => (minuteSeconds - time) | 0;
+const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
+// export default function App() {
+  const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
+  const endTime = stratTime + 243248; // use UNIX timestamp in seconds
+
+  const remainingTime = endTime - stratTime;
+  // const days = Math.ceil(remainingTime / daySeconds);
+  //const daysDuration = days * daySeconds;
+
+
+
+ return (
     <div className="App-header" id="Dash">
-      <center>
         <header>
           <h3>
             <center>
               Dashboard
             </center>
           </h3>
-        </header>
+         
+        <div className="timer">
+          <div class="row">
+          <div class="column">
+         <CountdownCircleTimer
+         {...timerProps}
+         colors={[["#EF798A"]]}
+        duration={hourSeconds}
+        initialRemainingTime={remainingTime % hourSeconds}
+        onComplete={(totalElapsedTime) => [
+        remainingTime - totalElapsedTime > minuteSeconds
+        ]}
+>
+        {({ elapsedTime }) =>
+        renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))
+        }
+        </CountdownCircleTimer>
+        </div>
+       
+      <div class="column">
+      <CountdownCircleTimer
+        {...timerProps}
+        colors={[["#218380"]]}
+        duration={minuteSeconds}
+        initialRemainingTime={remainingTime % minuteSeconds}
+        onComplete={(totalElapsedTime) => [
+        remainingTime - totalElapsedTime > 0
+        ]}
+      >
+        {({ elapsedTime }) =>
+          renderTime("seconds", getTimeSeconds(elapsedTime))
+        }
+      </CountdownCircleTimer>
+      </div>
+
+
+      </div>
+      </div>
+      </header>
+       
+       
         <body>
-          {/* Detection Section Starts here*/}
+            {/* Detection Section Starts here*/}
           <Detection>
+        
+           </Detection>
+           {/*Detection Section ends here */}
+  
 
-          </Detection>
-          {/*Detection Section ends here */}
+        
+        {/* Form Section Starts here */}
+        
+          <QuestionPage>
 
-          {/* Form Section Starts here */}
-          <div>
-            <QuestionPage>
-
-            </QuestionPage>
-          </div>
-          {/* Form Section ends here */}
-          <Button variant="contained" onClick={onAccept}>Submit</Button>
+          </QuestionPage>
+    
+        {/* Form Section ends here */}
+        
+      <br />
+        <center><Button variant="contained" onClick={onAccept}>Submit</Button></center>
         </body>
-      </center>
     </div>
   )
 }
