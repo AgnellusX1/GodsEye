93cd14587a1df9a1ecd55edfb2d12c84a7d91c16
Sancia Dcunha <sanciadcunha15@gmail.com>
2021-03-31 00:51:31 +0530

    FIXED: Questions.js

--- gods-eye/src/components/Questions.js
+++ gods-eye/src/components/Questions.js
@@ -93,7 +93,7 @@ export default class Timer extends Component {
                 }*/}
 
 
-                <div className="qsection"><iframe src={form_link} id='form' width="640" height="1338" frameBorder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                <div className="qsection"><iframe src={form_link} id='form' width="650" height="700" frameBorder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                 </div>
             </div>
 
