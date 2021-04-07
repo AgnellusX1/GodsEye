9d647922972742fdfdb459200a1b95cd90db34e8
AgnellusX1 <43168803+AgnellusX1@users.noreply.github.com>
2021-04-07 13:03:37 +0530

    FIXED: onBlur | Restructured

--- gods-eye/src/components/Dashboard.js
+++ gods-eye/src/components/Dashboard.js
@@ -6,10 +6,11 @@ import { Button } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import swal from 'sweetalert';
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
-var count_fullscreen=0;
-var count_tabchange=0;
+var count_fullscreen = 0;
+var count_tabchange = 0;
 const minuteSeconds = 60;
 const hourSeconds = 3600;
+
 const Dashboard = () => {
   //Disable Right click
   if (document.addEventListener) {
@@ -23,10 +24,8 @@ const Dashboard = () => {
       swal("Changed Tab Detected", "Action has been Recorded", "error");
       // the page is hidden
       //var count_tabchange = sessionStorage.getItem("count_tabchange")
-      count_tabchange=count_tabchange+1
+      count_tabchange = count_tabchange + 1
       sessionStorage.setItem("count_tabchange", count_tabchange);
-      
-
     } else {
       // the page is visible
     }
@@ -34,21 +33,11 @@ const Dashboard = () => {
   document.addEventListener("visibilitychange", handleVisibilityChange, false);
 
   //To make sure the user does not open any other App or lose Focus from the test Window
-  var i= 0;
-  
-    window.onfocus = function () {
-    console.log("Show exit");
-    swal("You left the Secure Exam Window","Donot Click Out of the Form" , "Action has been Recorded", "error");
-    i = i+1;
-    if(i>3)
-    history.push('/thankyou')
-      // return <Redirect to='/thankyou'/>
-    
-    }
-    
-  
-  
-  
+  var i = 0;
+
+  window.onblur = function () {
+    swal("Escaped Secure Window", "Action has been Recorded", "error");
+  }
 
   const history = useHistory();
   function onAccept() {
@@ -65,16 +54,13 @@ const Dashboard = () => {
     if (document.fullscreenElement) {
       console.log(`Element: ${document.fullscreenElement.id} entered full-screen mode.`);
     } else {
-      
       history.push("/fullscreenalert")
       //var count_fullscreen = sessionStorage.getItem("count_fullscreen")
-      count_fullscreen=count_fullscreen+1;
+      count_fullscreen = count_fullscreen + 1;
       console.log("count_fullscreen")
       console.log(count_fullscreen)
       sessionStorage.setItem("count_fullscreen", count_fullscreen);
-
     }
-    
   });
 
   //For timer
@@ -82,7 +68,7 @@ const Dashboard = () => {
     isPlaying: true,
     size: 120,
     strokeWidth: 6
-  };  
+  };
   const renderTime = (dimension, time) => {
     return (
       <div className="time-wrapper">
@@ -91,100 +77,86 @@ const Dashboard = () => {
       </div>
     );
   };
-//For minutes, seconds
-const getTimeSeconds = (time) => (minuteSeconds - time) | 0;
-const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
-const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
-const endTime = stratTime + 243248; 
-const remainingTime = endTime - stratTime;
+  //For minutes, seconds
+  const getTimeSeconds = (time) => (minuteSeconds - time) | 0;
+  const getTimeMinutes = (time) => ((time % hourSeconds) / minuteSeconds) | 0;
+  const stratTime = Date.now() / 1000; // use UNIX timestamp in seconds
+  const endTime = stratTime + 243248;
+  const remainingTime = endTime - stratTime;
 
+  return (
 
- return (
-     
-  
-  
-    
     <div className="App-header" id="Dash">
-        <header>
-          {/* <h3>
+      <header>
+        {/* <h3>
             <center>
               Dashboard
             </center>
           </h3> */}
-         
+
         <div className="timer">
           <div class="row">
 
-          <div class="column">
-         <CountdownCircleTimer
-         {...timerProps}
-         colors={[["#EF798A"]]}
-        duration={hourSeconds}
-        initialRemainingTime={remainingTime % hourSeconds}
-        onComplete={(totalElapsedTime) => [
-        remainingTime - totalElapsedTime > minuteSeconds
-        ]}
->
-        {({ elapsedTime }) =>
-        renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))
-        }
-        </CountdownCircleTimer>
-        </div>
-       
-      <div class="column">
-      <CountdownCircleTimer
-        {...timerProps}
-        colors={[["#218380"]]}
-        duration={minuteSeconds}
-        initialRemainingTime={remainingTime % minuteSeconds}
-        onComplete={(totalElapsedTime) => [
-        remainingTime - totalElapsedTime > 0
-        ]}
-      >
-        {({ elapsedTime }) =>
-          renderTime("seconds", getTimeSeconds(elapsedTime))
-        }
-      </CountdownCircleTimer>
-      </div>
+            <div class="column">
+              <CountdownCircleTimer
+                {...timerProps}
+                colors={[["#EF798A"]]}
+                duration={hourSeconds}
+                initialRemainingTime={remainingTime % hourSeconds}
+                onComplete={(totalElapsedTime) => [
+                  remainingTime - totalElapsedTime > minuteSeconds
+                ]}
+              >
+                {({ elapsedTime }) =>
+                  renderTime("minutes", getTimeMinutes(hourSeconds - elapsedTime))
+                }
+              </CountdownCircleTimer>
+            </div>
 
-
-      </div>
-      </div>
-
-      <div className="detect">
-        {/* Detection Section Starts here*/}
-          <Detection> 
-        
-          </Detection>
-        {/*Detection Section ends here */}
-
+            <div class="column">
+              <CountdownCircleTimer
+                {...timerProps}
+                colors={[["#218380"]]}
+                duration={minuteSeconds}
+                initialRemainingTime={remainingTime % minuteSeconds}
+                onComplete={(totalElapsedTime) => [
+                  remainingTime - totalElapsedTime > 0
+                ]}
+              >
+                {({ elapsedTime }) =>
+                  renderTime("seconds", getTimeSeconds(elapsedTime))
+                }
+              </CountdownCircleTimer>
+            </div>
           </div>
+        </div>
+
+        <div className="detect">
+          {/* Detection Section Starts here*/}
+          <Detection>
+
+          </Detection>
+          {/*Detection Section ends here */}
+        </div>
 
         <div className="text">
           <p>Remaining Time!!</p>
         </div>
-           
-  
 
         <div className="quest">
-        {/* Form Section Starts here */}
+          {/* Form Section Starts here */}
           <QuestionPage>
 
           </QuestionPage>
-    
-        {/* Form Section ends here */}
-        
-        
+          {/* Form Section ends here */}
         </div>
-        
-      <div className="button">
-        <p>Submit here!!</p>
-        <center><Button variant="contained" onClick={onAccept}>Submit</Button></center>
-         </div> 
+
+        <div className="button">
+          <p>Submit here!!</p>
+          <center><Button variant="contained" onClick={onAccept}>Submit</Button></center>
+        </div>
       </header>
-       
-       
-       
+
     </div>
   )
 }
