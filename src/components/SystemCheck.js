5d52a9b3979dc8e8d869c53367022d972e9821b0
Agnellus Fernandes <agnellusfernandes@gmail.com>
2021-05-14 05:04:56 +0530

    ADDED: Support For Firefox | System Check Fixes

--- src/components/SystemCheck.js
+++ src/components/SystemCheck.js
@@ -1,146 +1,208 @@
 import { Button } from '@material-ui/core';
 import React from 'react'
-import {useHistory} from 'react-router-dom';
-import browser from 'browser-detect';
-import SpeedTester from 'browser-speed-test';
+import { useHistory } from 'react-router-dom';
 import "./SystemCheck.css"
 import icon from "./icon.png"
+import DetectRTC from 'detectrtc';
+import swal from 'sweetalert';
+
+const NetworkSpeed = require('network-speed');  // ES5
+const testNetworkSpeed = new NetworkSpeed();
+
+var buttonViewDisabled = true
+
+function ValidateCheck() {
+
+  var isAllowed = false;
+
+
+  // Network Check
+  var netSpeedVar = sessionStorage.getItem("netspeed");
+  if (netSpeedVar > 2) {
+    isAllowed = true
+  }
+
+
+  // Browser Check
+  if (DetectRTC.browser.isChrome) {  //.................................Chrome
+    // If Browser is Chrome
+    if (DetectRTC.browser.version > 80) {
+      // If the Browser is updated
+      isAllowed = true
+    } else {
+      // If browser is not Updated
+      swal("Please Update Browser or Try a Different Browser")
+      isAllowed = false
+    }
+  } if (DetectRTC.browser.isFirefox) { //.................................Firefox
+    // If Browser is Chrome
+    if (DetectRTC.browser.version > 60) {
+      // If the Browser is updated
+      isAllowed = true
+    } else {
+      // If browser is not Updated
+      swal("Please Update Browser or Try a Different Browser")
+      isAllowed = false
+    }
+  } if (DetectRTC.browser.isSafari) {  //.................................Safari
+    // If Browser is Chrome
+    if (DetectRTC.browser.version > 12) {
+      // If the Browser is updated
+      isAllowed = true
+    } else {
+      // If browser is not Updated
+      swal("Please Update Browser or Try a Different Browser")
+      isAllowed = false
+    }
+  } if (DetectRTC.browser.isOpera) { //.................................Opera
+    // If Browser is Chrome
+    if (DetectRTC.browser.version > 60) {
+      // If the Browser is updated
+      isAllowed = true
+    } else {
+      // If browser is not Updated
+      swal("Please Update Browser or Try a Different Browser")
+      isAllowed = false
+    }
+  } if (DetectRTC.browser.isEdge) { //.................................Edge
+    // If Browser is Chrome
+    if (DetectRTC.browser.version > 80) {
+      // If the Browser is updated
+      isAllowed = true
+    } else {
+      // If browser is not Updated
+      swal("Please Update Browser or Try a Different Browser")
+      isAllowed = false
+    }
+  }
+
+  // OS Check
+  // Note: Not sure WHat we are going to do with This
+
+  // Camera Permission
+  DetectRTC.load(function () {
+
+    const webcam = DetectRTC.isWebsiteHasWebcamPermissions;
+    if (!webcam) {
+      navigator.getUserMedia = navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia;
+
+      var video = document.querySelector("#videoElement");
+      if (navigator.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+          .then(function (stream) {
+            video.srcObject = stream;
+          })
+
+          .catch(function (err0r) {
+            console.log("Something went wrong!");
+          });
+      }
+    }
+
+  });
+
+
+
+  const webcam = DetectRTC.isWebsiteHasWebcamPermissions;
+  console.log(webcam)
+  if (webcam) {
+    isAllowed = true;
+  } else {
+    isAllowed = false;
+  }
+
+
+  // Final Approval
+
+  if (isAllowed) {
+    buttonViewDisabled = false
+  } else {
+    buttonViewDisabled = true
+  }
+
+
+}
 
 const SystemCheck = () => {
 
-function handleClick(){
-  history.push("/validate");
-}
-    
-    var elem = document.documentElement;
-
-/* View in fullscreen */
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-    history.push("/instructions")
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-    history.push("/instructions")
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-    history.push("/instructions")
+  // Network Speed Check
+  async function getNetworkDownloadSpeed() {
+    const baseUrl = 'https://eu.httpbin.org/stream-bytes/500000';
+    const fileSizeInBytes = 500000;
+    const speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
+    return speed
   }
-}
+  getNetworkDownloadSpeed().then((value) => { sessionStorage.setItem("netspeed", value["mbps"]) })
 
-//browser speed test
-const tester = new SpeedTester({ });
-tester.start();
-console.log('Browserspeed:', tester)
+  // System Detections Package
+  var DetectRTC = require('detectrtc');
 
-//Browser detection
-// const result = browser();
-// const result= window.navigator.userAgent
-// const result=window. navigator.appVersion
-// console.log('Browserdetect:',result);
+  ValidateCheck();
 
-//Network speed
-const net = `${navigator.connection.downlink}`
-console.log('Networkspeed', net);
-<div id="debugDiv"></div>
+  function handleClick() {
+    history.push("/validate");
+  }
 
-const history=useHistory();
+  var elem = document.documentElement;
 
-//Camera Detection
-var DetectRTC = require('detectrtc');
-DetectRTC.load(function(){
-
-    var webcam=DetectRTC.isWebsiteHasWebcamPermissions;
-    if (webcam===false){
-         
-        var video = document.querySelector("#videoElement");
-        if (navigator.mediaDevices.getUserMedia) {
-            navigator.mediaDevices.getUserMedia({ video: true })
-              .then(function (stream) {
-                video.srcObject = stream;
-              })
-              
-              .catch(function (err0r) {
-                console.log("Something went wrong!");
-              });
-          }
+  /* View in fullscreen */
+  function openFullscreen() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+      history.push("/instructions")
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+      history.push("/instructions")
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+      history.push("/instructions")
     }
-
-});
-
-var aggi = false;
-//browser
-var browser=DetectRTC.browser.version
-if(browser<= 86){
-    console.log("Update Browser");
-    aggi = true;
-}
-//Net Speed
-if(net <= 0.3 ){
-    console.log("Please Continue", net);
-    aggi = true;
-}
-
-//Cam Detection
-var webcam=DetectRTC.isWebsiteHasWebcamPermissions;
-if (webcam===false){
-  console.log("Not Activated",webcam);
-  aggi = true;
-  console.log("Activated Successfully",webcam)
-}
-
-//Permission to Camera
-// function ActivateCam(){
-//   var now = Date.now();
-// navigator.mediaDevices.getUserMedia({video: true})
-// .then(function(stream) {
-//   console.log('Got stream, time diff :', Date.now() - now);
-//   handleClick();
-// })
-
-// }
+  }
+  const history = useHistory();
 
 
   return (
     <body class="App-header">
-  <div class="main">
-    <p class="sign" align="center">System Compatibility Check</p>
+      <div class="main">
+        <p class="sign" align="center">System Compatibility Check</p>
         <table align="center">
-            <tbody><tr>
-                <td class="text-center">
-                    <div>
-                        <img src={icon} id="classIcon" />
-                    </div>
-                </td>
-                <td>
-                    <ul>
-                        <li class="test">
-                            <span ><b>OS:</b>  {"- " + JSON.stringify( DetectRTC.osName,null,2).slice(1,-1)+" " + JSON.stringify(DetectRTC.osVersion,null,0).slice(1,-1)} </span>
-                        </li>
-                        <li class="test">
-                            <span><b>Browser:</b> {"- " + JSON.stringify(DetectRTC.browser.name === 'Edge' || 'Chrome' || 'Firefox',null,2).slice(1,-1)+" "+ JSON.stringify(DetectRTC.browser.version) } </span>
-                        </li>
-                        <li class="test">
-                            <span><b>Internet Speed:</b> {"- " + JSON.stringify(net,null,2 ).slice(1,-1) + " Mbps" } </span>
-                        </li>
-                         <li class="test">
-                         <span><b>Webcam:</b> {"- "+JSON.stringify(DetectRTC.isWebsiteHasWebcamPermissions)} </span>
-                        </li>
-                        
-                    </ul>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+          <tbody><tr>
+            <td class="text-center">
+              <div>
+                <img src={icon} id="classIcon" />
+              </div>
+            </td>
+            <td>
+              <ul>
+                <li class="test">
+                  <span ><b>OS:</b>  {"- " + JSON.stringify(DetectRTC.osName, null, 2).slice(1, -1) + " " + JSON.stringify(DetectRTC.osVersion, null, 0).slice(1, -1)} </span>
+                </li>
+                <li class="test">
+                  <span><b>Browser:</b> {"- " + JSON.stringify(DetectRTC.browser.name === 'Edge' || 'Chrome' || 'Firefox', null, 2).slice(1, -1) + " " + JSON.stringify(DetectRTC.browser.version)} </span>
+                </li>
+                <li class="test">
+                  <span><b>Internet Speed:</b> {"- " + sessionStorage.getItem("netspeed") + " mbps"} </span>
+                </li>
+                <li class="test">
+                  <span><b>Webcam:</b> {"- " + JSON.stringify(DetectRTC.isWebsiteHasWebcamPermissions)} </span>
+                </li>
+
+              </ul>
+            </td>
+          </tr>
+          </tbody>
+        </table>
 
 
-    <center><Button disabled={aggi} variant='contained' onClick={openFullscreen}>Validate</Button></center>  
-    <br></br>
-    <center><Button variant='contained' onClick={handleClick}>Activate Your WebCam</Button></center>     
-                
-    </div>
-     
-</body>
+        <center><Button disabled={buttonViewDisabled} variant='contained' onClick={openFullscreen}>Validate</Button></center>
+        <br></br>
+        <center><Button variant='contained' onClick={handleClick}>Activate Your WebCam</Button></center>
+
+      </div>
+
+    </body>
 
   )
 }
